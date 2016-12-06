329ad6bf22f34cff1dbd4fce7e8c5a59cc0b7f5b
michaelwishlow <michael@pantheritconsulting.com>
2016-12-06 12:13:59 -0800

    Add and Initial Define Notes Controller

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,42 @@
+  class NotesController < ApplicationController
+  	 before_action :find_note, only: [:show, :edit, :update, :destroy]
+  	 
+				def index
+				end
+
+				def show
+				end
+
+				def new
+				  @note = Note.new
+				end
+
+				def create
+					 @note = Note.new(note_params)
+
+					 if @note.save
+					 		redirect_to @note
+					 else
+					 	 render 'new'
+					 end
+				end
+
+				def edit
+				end
+
+				def update
+				end
+
+				def destroy
+				end
+
+				private
+
+				def find_note
+					@note = Note.find(params[:id])
+				end
+
+				def note_params
+					 params.require(:note).permit(:title, :content)
+				end
+  end
