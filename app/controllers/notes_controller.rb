09d4fe0b2b6ed6216495a9f642971efd52382c21
michaelwishlow <michael@pantheritconsulting.com>
2016-12-07 11:09:23 -0800

    Edit & Delete Notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,6 +2,7 @@
   	 before_action :find_note, only: [:show, :edit, :update, :destroy]
   	 
 				def index
+					@notes = Note.all.order("created_at DESC")
 				end
 
 				def show
@@ -25,9 +26,16 @@
 				end
 
 				def update
+						if @note.update(note_params)
+								redirect_to @note
+						else
+								render 'edit'
+						end
 				end
 
 				def destroy
+						@note.destroy
+						redirect_to notes_path
 				end
 
 				private
