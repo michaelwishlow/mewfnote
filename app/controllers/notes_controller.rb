6e229f7063aa4c6815b54749410890d946f2330d
michaelwishlow <michael@pantheritconsulting.com>
2016-12-08 11:16:43 -0800

    Modify Index Actions depending on User signin status

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,50 +1,50 @@
   class NotesController < ApplicationController
   	 before_action :find_note, only: [:show, :edit, :update, :destroy]
   	 
-				def index
-					@notes = Note.all.order("created_at DESC")
-				end
+					def index
+						@notes = Note.where(user_id: current_user)
+					end
 
-				def show
-				end
+					def show
+					end
 
-				def new
-				  @note = current_user.notes.build
-				end
+					def new
+					  @note = current_user.notes.build
+					end
 
-				def create
-					 @note = current_user.notes.build(note_params)
+					def create
+						 @note = current_user.notes.build(note_params)
 
-					 if @note.save
-					 		redirect_to @note
-					 else
-					 	 render 'new'
-					 end
-				end
+						 if @note.save
+						 		redirect_to @note
+						 else
+						 	 render 'new'
+						 end
+					end
 
-				def edit
-				end
+					def edit
+					end
 
-				def update
-						if @note.update(note_params)
-								redirect_to @note
-						else
-								render 'edit'
-						end
-				end
+					def update
+							if @note.update(note_params)
+									redirect_to @note
+							else
+									render 'edit'
+							end
+					end
 
-				def destroy
-						@note.destroy
-						redirect_to notes_path
-				end
+					def destroy
+							@note.destroy
+							redirect_to notes_path
+					end
 
-				private
+					private
 
-				def find_note
-					@note = Note.find(params[:id])
-				end
+					def find_note
+						@note = Note.find(params[:id])
+					end
 
-				def note_params
-					 params.require(:note).permit(:title, :content)
-				end
+					def note_params
+						 params.require(:note).permit(:title, :content)
+					end
   end
